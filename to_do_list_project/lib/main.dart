6a2721576a1ae72867b16d3f7abe1d5201294dcf
gilsonDias92 <gilsinho.1992@gmail.com>
2019-11-30 15:20:29 -0300

    Inicialização do projeto, criação dos primeiros widgets, MyApp e MyHome.

---
+++ to_do_list_project/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+// método que inicia o app
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // context tem infos sobre a localiz da
+    // arvore q o widget é renderizado
+
+    return MaterialApp(
+      title: 'To-Do List',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  // statefulwidget implementa metodo createState
+  // devolvemos um estado e criamos a classe  
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomeState();
+  }
+}
+
+class MyHomeState extends State<MyHome> {
+  // stateful retorna um state
+  // state executa o build
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
