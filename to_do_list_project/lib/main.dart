6d7a13d74e53c87ed438a6a046909b2f872526b4
gilsonDias92 <gilsinho.1992@gmail.com>
2019-11-30 15:40:48 -0300

    Iniciando a renderização do corpo do app

--- to_do_list_project/lib/main.dart
+++ to_do_list_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 main() => runApp(MyApp());
 // método que inicia o app
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
 
 class MyHome extends StatefulWidget {
   // statefulwidget implementa metodo createState
-  // devolvemos um estado e criamos a classe  
+  // devolvemos um estado e criamos a classe
 
   @override
   State<StatefulWidget> createState() {
@@ -31,9 +32,34 @@ class MyHomeState extends State<MyHome> {
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold cria a estrutura para um layout
     return Scaffold(
+      // O scaffold recebe parametros para criacão da tela
+
+      // renderiza a barra no topo do app
       appBar: AppBar(
+        //Widget AppBar recebe parametros
         title: Text('To-Do List'),
+        // title nao pode receber texto, tem q receber o
+        // widget Text com a string dentro...
+      ),
+
+      // renderiza o corpo do app e dentro dele os parametros
+      body: Container(
+        // widgets q precisaam receber um filho, se for mais de um recebe  children
+        // renderiza widgets na forma vertical
+        // column rece children pois pode receber varios widgets
+        child: Column(
+          children: <Widget>[
+            Form(
+              // prop child é obrigatoria!!
+
+              // row renderiza na forma horizontal, um ao lado do outro.
+              // iremos colocar um campo de input e um botao lado a lado
+              child: Row(),
+            )
+          ],
+        ),
       ),
     );
   }
